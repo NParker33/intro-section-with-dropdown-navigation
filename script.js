d384cbcecf52c249859e09898a100648d8ac9c3f
Nicole Parker <games.signups.etc@gmail.com>
2022-07-12 11:39:13 -0400

    initial commit - mobile design

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const containerMenu = document.querySelector(".menu-container");
+const menu = document.querySelector(".nav-menu");
+
+const btnHamburgerMenu = document.querySelector(".menu-btn");
+const btnCloseMenu = document.querySelector(".close-btn");
+const btnDownArrow = Array.from(document.querySelectorAll(".arrow-icon"));
+
+btnHamburgerMenu.addEventListener("click", () => containerMenu.classList.remove("hidden"));
+btnCloseMenu.addEventListener("click", () => containerMenu.classList.add("hidden"));
+
+btnDownArrow.forEach(btn =>
+  btn.addEventListener("click", () => {
+    console.log("ARROW ICON");
+    btn.nextElementSibling.classList.toggle("hidden");
+  })
+);
